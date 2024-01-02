d53d0bad4bbb193a5993515b6929d2375532d5da
erenguney <erenvbn@gmail.com>
2024-01-02 17:13:38 +0300

    Added and edited webpack.config.js and package.json

---
+++ wwwroot/js/webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+    }),
+  ],
+};
