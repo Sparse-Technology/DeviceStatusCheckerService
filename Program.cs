bca50d83ee28348b44e3e3c05dcbe4f3ad96e6e8
Ibrahim Ethem Gursoy <ibrahim.gursoy@sparsetechnology.com>
2024-01-04 18:20:24 +0300

    Check network interface is exist

--- Program.cs
+++ Program.cs
@@ -1,9 +1,25 @@
 using DeviceStatusCheckerService.Services;
 using Microsoft.Extensions.FileProviders;
+using System.Net.NetworkInformation;
 using System.Text.Json.Serialization;
 
 ThreadPool.SetMaxThreads(64, 128);
 var builder = WebApplication.CreateBuilder(args);
+
+var networkInterface = builder.Configuration.GetValue<string>("AppConfiguration:NetworkInterface")?.Trim();
+var interfaces = NetworkInterface.GetAllNetworkInterfaces().Where(i => i.OperationalStatus == OperationalStatus.Up);
+if (!interfaces.Where(i => i.Name == networkInterface).Any())
+{
+    Console.WriteLine($"Network interface '{networkInterface}' not found or not up. Please check your appsettings.json file.");
+    Console.WriteLine("Available network interfaces:");
+    Console.WriteLine($"  {"Name", 30}: Description\n----------------------------------------------");
+    foreach (var i in interfaces)
+        Console.WriteLine($"- {i.Name, 30}: {i.Description}");
+
+    const int ERROR_BAD_ARGUMENTS = 0xA0;
+    Environment.Exit(ERROR_BAD_ARGUMENTS);
+}
+
 builder.Services.AddSingleton<DeviceManager>();
 builder.Services.AddSingleton<CheckerService>();
 builder.Services.AddHttpClient();
