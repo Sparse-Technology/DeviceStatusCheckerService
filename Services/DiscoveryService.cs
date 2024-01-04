a136918de1dd3052bcb7869870a98bba6e474103
Ibrahim Ethem Gursoy <ibrahim.gursoy@sparsetechnology.com>
2024-01-04 18:24:16 +0300

    Enhance discovery process with configurable timeout and continuous search
    Key improvements:
    
    - Added a configurable timeout for the device discovery process (defaulting to 60 seconds).
    - Modified StartDiscovery method in DiscoveryService to accept the timeout value.
    - Implemented a continuous device search loop with periodic searches based on the timeout.
    - Added logging messages to indicate when device searches are initiated.
    - Minor code formatting adjustments for readability.

--- Services/DiscoveryService.cs
+++ Services/DiscoveryService.cs
@@ -6,15 +6,19 @@ namespace DeviceStatusCheckerService.Services
     {
         private SsdpDeviceLocator? _deviceLocator;
 
-        public void StartDiscovery(string bindIp, EventHandler<DeviceAvailableEventArgs> cb)
+        public void StartDiscovery(string bindIp, EventHandler<DeviceAvailableEventArgs> cb, int timeout = 60000)
         {
             _deviceLocator = new SsdpDeviceLocator(bindIp) { NotificationFilter = "upnp:rootdevice" };
             _deviceLocator.DeviceAvailable += cb;
+            _deviceLocator.StartListeningForNotifications();
 
             new Thread(() =>
             {
-                _deviceLocator.StartListeningForNotifications();
-                _deviceLocator.SearchAsync();
+                while (true) {
+                    Console.WriteLine("***************************\n** Searching for devices **\n***************************");
+                    _ = _deviceLocator.SearchAsync();
+                    Thread.Sleep(timeout);
+                }
             }).Start();
         }
 
