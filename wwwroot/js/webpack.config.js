6275a63f711268da1c156735df6d0014b6f9de98
erenguney <eren.guney@sparsetechnology.com>
2024-02-02 13:15:42 +0300

    C#:DSCS: Edited webpack.config to create a library

--- wwwroot/js/webpack.config.js
+++ wwwroot/js/webpack.config.js
@@ -3,29 +3,30 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: {
-    main: "./src/main.js",
-  },
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
+    entry: {
+        main: "./src/Classes/DeviceManagement.js",
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].bundle.js",
+        library: "DeviceManagementLib",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].bundle.css",
+        }),
     ],
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-      "window.jQuery": "jquery",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].bundle.css",
-    }),
-  ],
 };
