4dd3f4c7f43f6d41508e00ff3f8a1d85a04fabae
Ibrahim Ethem Gursoy <ibrahim.gursoy@sparsetechnology.com>
2024-02-03 12:58:40 +0300

    Configuration variables are populated from environment variables

--- Program.cs
+++ Program.cs
@@ -6,19 +6,63 @@ using System.Text.Json.Serialization;
 ThreadPool.SetMaxThreads(64, 128);
 var builder = WebApplication.CreateBuilder(args);
 
+#region Configuration
+if (Environment.GetEnvironmentVariable("Kestrel_Endpoints_Http_Url") is string kestrelEndpointsHttpUrl)
+    builder.Configuration["Kestrel:Endpoints:Http:Url"] = kestrelEndpointsHttpUrl;
+if (Environment.GetEnvironmentVariable("ServerDescription_Uuid") is string serverDescription_Uuid)
+    builder.Configuration["AppConfiguration:ServerDescription:Uuid"] = serverDescription_Uuid;
+if (Environment.GetEnvironmentVariable("ServerDescription_FriendlyName") is string serverDescription_FriendlyName)
+    builder.Configuration["AppConfiguration:ServerDescription:FriendlyName"] = serverDescription_FriendlyName;
+if (Environment.GetEnvironmentVariable("ServerDescription_Version") is string serverDescription_Version)
+    builder.Configuration["AppConfiguration:ServerDescription:Version"] = serverDescription_Version;
+if (Environment.GetEnvironmentVariable("ServerDescription_Manufacturer") is string serverDescription_Manufacturer)
+    builder.Configuration["AppConfiguration:ServerDescription:Manufacturer"] = serverDescription_Manufacturer;
+if (Environment.GetEnvironmentVariable("ServerDescription_ModelName") is string serverDescription_ModelName)
+    builder.Configuration["AppConfiguration:ServerDescription:ModelName"] = serverDescription_ModelName;
+if (Environment.GetEnvironmentVariable("ServerDescription_SerialNumber") is string serverDescription_SerialNumber)
+    builder.Configuration["AppConfiguration:ServerDescription:SerialNumber"] = serverDescription_SerialNumber;
+if (Environment.GetEnvironmentVariable("DiscoveryTimeout") is string discoveryTimeout)
+    builder.Configuration["AppConfiguration:DiscoveryTimeout"] = discoveryTimeout;
+if (Environment.GetEnvironmentVariable("EnableDiscovery") is string enableDiscovery)
+    builder.Configuration["AppConfiguration:EnableDiscovery"] = enableDiscovery;
+if (Environment.GetEnvironmentVariable("EnableCheckPing") is string enableCheckPing)
+    builder.Configuration["AppConfiguration:EnableCheckPing"] = enableCheckPing;
+if (Environment.GetEnvironmentVariable("EnableCheckPort") is string enableCheckPort)
+    builder.Configuration["AppConfiguration:EnableCheckPort"] = enableCheckPort;
+if (Environment.GetEnvironmentVariable("EnableCheckOnvifMedia") is string enableCheckOnvifMedia)
+    builder.Configuration["AppConfiguration:EnableCheckOnvifMedia"] = enableCheckOnvifMedia;
+if (Environment.GetEnvironmentVariable("EnableCheckRTSPLinks") is string enableCheckRTSPLinks)
+    builder.Configuration["AppConfiguration:EnableCheckRTSPLinks"] = enableCheckRTSPLinks;
+if (Environment.GetEnvironmentVariable("EnableFFmpegRTSPStreamTest") is string enableFFmpegRTSPStreamTest)
+    builder.Configuration["AppConfiguration:EnableFFmpegRTSPStreamTest"] = enableFFmpegRTSPStreamTest;
+if (Environment.GetEnvironmentVariable("ThumbnailsPath") is string thumbnailsPath1)
+    builder.Configuration["AppConfiguration:ThumbnailsPath"] = thumbnailsPath1;
+if (Environment.GetEnvironmentVariable("DefaultDeviceJsonPath") is string defaultDeviceJsonPath)
+    builder.Configuration["AppConfiguration:DefaultDeviceJsonPath"] = defaultDeviceJsonPath;
+if (Environment.GetEnvironmentVariable("NetworkInterface") is string networkInterface1)
+    builder.Configuration["AppConfiguration:NetworkInterface"] = networkInterface1;
+
 var networkInterface = builder.Configuration.GetValue<string>("AppConfiguration:NetworkInterface")?.Trim();
+if (Environment.GetEnvironmentVariable("BIND_INTERFACE") is string bindInterface)
+{
+    networkInterface = bindInterface;
+    builder.Configuration["AppConfiguration:NetworkInterface"] = bindInterface;
+}
+
 var interfaces = NetworkInterface.GetAllNetworkInterfaces().Where(i => i.OperationalStatus == OperationalStatus.Up);
 if (!interfaces.Where(i => i.Name == networkInterface).Any())
 {
     Console.WriteLine($"Network interface '{networkInterface}' not found or not up. Please check your appsettings.json file.");
     Console.WriteLine("Available network interfaces:");
-    Console.WriteLine($"  {"Name", 30}: Description\n----------------------------------------------");
+    Console.WriteLine($"  {"Name",30}: Description\n----------------------------------------------");
     foreach (var i in interfaces)
-        Console.WriteLine($"- {i.Name, 30}: {i.Description}");
+        Console.WriteLine($"- {i.Name,30}: {i.Description}");
 
     const int ERROR_BAD_ARGUMENTS = 0xA0;
     Environment.Exit(ERROR_BAD_ARGUMENTS);
 }
+Console.WriteLine($"Using configuration: {builder.Configuration.GetDebugView()}");
+#endregion
 
 builder.Services.AddSingleton<DeviceManager>();
 builder.Services.AddSingleton<CheckerService>();
--- README.md
+++ README.md
@@ -16,3 +16,28 @@ It provides detailed information about each device, including Last Checked Statu
 ### Export Menu for Advanced Users
   - Advanced Export Options: Advanced users can access a template engine for manual customization of label parts.
   - Template Engine: Input the desired template, select devices, and customize label parts for a tailored JSON output.
+
+### Start with options
+
+Set up the project by running the following environment variables:
+
+```bash
+export Kestrel_Endpoints_Http_Url="http://*:80"
+export ServerDescription_Uuid="00000000-0000-0000-0000-000000000000"
+export ServerDescription_FriendlyName="Device Status Checker"
+export ServerDescription_Version="v0.0.1"
+export ServerDescription_Manufacturer="Sparse Lab"
+export ServerDescription_ModelName="Device Status Checker"
+export ServerDescription_SerialNumber="00000000"
+export DiscoveryTimeout=60000
+export EnableDiscovery=true
+export EnableCheckPing=true
+export EnableCheckPort=true
+export EnableCheckOnvifMedia=true
+export EnableCheckRTSPLinks=true
+export EnableFFmpegRTSPStreamTest=true
+export ThumbnailsPath="/tmp/thumbnails/"
+export DefaultDeviceJsonPath="device_list.json"
+export NetworkInterface="eth0"
+export BIND_INTERFACE="eth0"
+```
