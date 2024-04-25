4d384da26dbbb643a4aac0f9ca35c91692e39c98
Ibrahim Ethem Gursoy <ibrahim.gursoy@sparsetechnology.com>
2024-04-25 18:10:37 +0300

    Add SetLoginUsers API endpoint to DeviceController.cs

--- Controllers/DeviceController.cs
+++ Controllers/DeviceController.cs
@@ -9,17 +9,26 @@ namespace DeviceStatusCheckerService.Controllers
     {
         private readonly ILogger<DeviceController> _logger;
         private readonly DeviceManager _deviceManager;
+        private readonly CheckerService _checkerService;
 
-        public DeviceController(ILogger<DeviceController> logger, DeviceManager deviceManager)
+        public DeviceController(ILogger<DeviceController> logger, DeviceManager deviceManager, CheckerService checkerService)
         {
             _logger = logger;
             _deviceManager = deviceManager;
+            _checkerService = checkerService;
         }
 
         [HttpGet("ListDevices")]
         public async Task<List<Models.DeviceModel>> ListDevices()
         {
             return await Task.Run(() => _deviceManager.Devices);
+        }
+
+        [HttpPost("SetLoginUsers")]
+        public IActionResult SetEstimatedAuths(List<Models.LoginUserModel> users)
+        {
+            _checkerService.SetLoginUsers(users);
+            return Ok();
         }
     }
 }
