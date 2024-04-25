0acecbacea6678801b7faecced843fbd7735a6ec
Ibrahim Ethem Gursoy <ibrahim.gursoy@sparsetechnology.com>
2024-04-25 18:12:07 +0300

    Add new PTZ API endpoints to DeviceController.cs

--- Controllers/DeviceController.cs
+++ Controllers/DeviceController.cs
@@ -30,5 +30,110 @@ namespace DeviceStatusCheckerService.Controllers
             _checkerService.SetLoginUsers(users);
             return Ok();
         }
+
+        [HttpPost("CheckDevicePTZSupport")]
+        public async Task<IActionResult> CheckDevicePTZSupportAsync(Models.LoginUserModel auth)
+        {
+            try
+            {
+                if (auth.ip == null || auth.user == null || auth.pass == null)
+                    return BadRequest("Required auth parameters are missing");
+
+                if (await Helper.CheckDevicePTZSupportAsync(auth.ip, auth.user, auth.pass))
+                    return Ok();
+                else
+                    return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpPost("ListDeviceOnvifProfiles")]
+        public async Task<IActionResult> ListDeviceOnvifProfilesAsync(Models.LoginUserModel auth)
+        {
+            try
+            {
+                if (auth.ip == null || auth.user == null || auth.pass == null)
+                    return BadRequest("Required auth parameters are missing");
+
+                var profiles = await Helper.GetOnvifProfilesAsync(auth.ip, auth.user, auth.pass);
+                if (profiles != null)
+                    return Ok(profiles);
+                else
+                    return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpPost("StartPTZContinuousMove")]
+        public async Task<IActionResult> StartPTZContinuousMoveAsync(Models.StartPTZModel model)
+        {
+            try
+            {
+                if (model.auth == null || model.ptz == null || string.IsNullOrEmpty(model.profileToken))
+                    return BadRequest("Invalid parameters");
+
+                if (model.auth.ip == null || model.auth.user == null || model.auth.pass == null)
+                    return BadRequest("Required auth parameters are missing");
+
+                if (await Helper.PTZContinuousMoveAsync(model.auth.ip, model.auth.user, model.auth.pass, model.profileToken, model.ptz))
+                    return Ok();
+                else
+                    return Problem("Cannot start PTZ continuous move");
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpPost("StopPTZContinuousMove")]
+        public async Task<IActionResult> StopPTZContinuousMoveAsync(Models.StopPTZModel model)
+        {
+            try
+            {
+                if (model.auth == null || string.IsNullOrEmpty(model.profileToken))
+                    return BadRequest("Invalid parameters");
+
+                if (model.auth.ip == null || model.auth.user == null || model.auth.pass == null)
+                    return BadRequest("Required auth parameters are missing");
+
+                if (await Helper.PTZStopAsync(model.auth.ip, model.auth.user, model.auth.pass, model.profileToken))
+                    return Ok();
+                else
+                    return Problem("Cannot stop PTZ continuous move");
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+        [HttpPost("GoToHomePosition")]
+        public async Task<IActionResult> GoToHomePositionAsync(Models.GoToHomePTZModel model)
+        {
+            try
+            {
+                if (model.auth == null || string.IsNullOrEmpty(model.profileToken))
+                    return BadRequest("Invalid parameters");
+
+                if (model.auth.ip == null || model.auth.user == null || model.auth.pass == null)
+                    return BadRequest("Required auth parameters are missing");
+
+                if (await Helper.GoToPTZHomeAsync(model.auth.ip, model.auth.user, model.auth.pass, model.profileToken))
+                    return Ok();
+                else
+                    return Problem("Cannot go to home position");
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
     }
 }
